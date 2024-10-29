326c2ef2d8f19caaf660ba159ba19cd840249141
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-10-29 10:05:26 +0100

    adding Exists and its Validation

--- src/main/java/ma/hariti/asmaa/survey/survey/validation/annotation/Exists.java
+++ src/main/java/ma/hariti/asmaa/survey/survey/validation/annotation/Exists.java
@@ -1,4 +1,21 @@
 package ma.hariti.asmaa.survey.survey.validation.annotation;
 
-public class Exists {
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import ma.hariti.asmaa.survey.survey.validation.validator.ExistsValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ExistsValidator.class)
+public @interface Exists {
+    String message() default "Resource does not exist";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    Class<?> entity();
 }
--- src/main/java/ma/hariti/asmaa/survey/survey/validation/validator/ExistsValidator.java
+++ src/main/java/ma/hariti/asmaa/survey/survey/validation/validator/ExistsValidator.java
@@ -1,4 +1,32 @@
 package ma.hariti.asmaa.survey.survey.validation.validator;
 
-public class ExistsValidator {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import ma.hariti.asmaa.survey.survey.validation.annotation.Exists;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ExistsValidator implements ConstraintValidator<Exists, Long> {
+    @Autowired
+    private EntityManager entityManager;
+    private Class<?> entity;
+
+    @Override
+    public void initialize(Exists constraintAnnotation) {
+        this.entity = constraintAnnotation.entity();
+    }
+
+    @Override
+    public boolean isValid(Long id, ConstraintValidatorContext context) {
+        if (id == null) return true;
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<?> query = cb.createQuery(entity);
+        Root<?> root = query.from(entity);
+        query.select(root.get("id"))
+                .where(cb.equal(root.get("id"), id));
+        return !entityManager.createQuery(query).setMaxResults(1).getResultList().isEmpty();
+    }
 }
